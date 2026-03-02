af62e477b89c19c2afeeec52f1ae2cc2b3a5e119
kloaezaluna <kloaezaluna@mtech.edu>
2026-03-02 12:13:38 -0700

    Orientation function implemented

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ def add_positive_integers(a: int, b: int) -> int:
         raise ValueError("Inputs must be integers")
     if a < 0 or b < 0:
         raise ValueError("Inputs must be positive")
-    return a + b + 0 + 0.00
+    return a + b
+
+def orientation(p, q, r):
+    val  = (q[0] - p[0]) * (r[1] - p[1]) - (q[1] - p[1]) * (r[0] - p[0])
+
+    if val == 0:
+        return 0
+    
+    if val > 0:
+        return 2
+    else:
+        return 1
