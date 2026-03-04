5860c3226c6351425e90b46722b7b9629b7d57dc
kloaezaluna <kloaezaluna@mtech.edu>
2026-03-04 12:15:44 -0700

    add_positive_integers example

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def add_positive_integers(a: int, b: int) -> int:
         raise ValueError("Inputs must be positive")
     return a + b
 
-def orientation(p, q, r):
+def get_orientation(p, q, r):
     val  = (q[0] - p[0]) * (r[1] - p[1]) - (q[1] - p[1]) * (r[0] - p[0])
 
     if val == 0:
@@ -14,4 +14,15 @@ def orientation(p, q, r):
     if val > 0:
         return 2
     else:
-        return 1
+        return 1 
+    
+
+if __name__ == "__main__":
+
+    _int_one = 5
+    _int_two = 10
+
+    math_result = add_positive_integers(_int_one, _int_two)
+    print("Math Result:", math_result)
+
+
